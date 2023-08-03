258fe9946b98c08bc3cb70697adfb9acd947c309
NajiyaShabeer <najishab@gmail.com>
2023-08-03 21:21:02 +0530

    first commit

---
+++ basic.js
@@ -0,0 +1,42 @@
+
+
+//var x=10;
+//var y=22;
+//var result=x+y;
+//console.log(result);
+//var result=x/y;
+//console.log(result);
+//var result=x%y;
+//console.log(result);
+
+
+
+//let student
+//{
+
+    //title='joe';
+    //age=18;
+   // isEligible=true;
+//}
+//console.log(student.title);
+
+let season=['summer','autumn','winter','spring'];
+console.log(season[2]);
+
+function sum(x,y)
+{
+    let z=x+y;
+    console.log("sum = "+z);
+}
+sum(10,20);
+
+
+
+
+
+
+
+
+
+
+
---
+++ loop.js
@@ -0,0 +1,47 @@
+//print 1 to 10 numbers
+
+for(let i=1;i<=10;i++)
+{
+    console.log(i);
+}
+//print array elements
+var x=['mango','grape','coconut'];
+for(let i=0;i<x.length+1;i++)
+console.log(x[i]);
+
+//for in loop
+var obj={name:'Najiya',place:"kozhikode"}
+for(let i in obj)
+{
+    console.log(obj[i]);
+}
+//for of loop
+var str='Amy'
+for(let i of str)
+{
+    console.log(i);
+}
+
+//while loop
+let i=1;
+while(i<=10);
+{
+    console.log(i);
+i++;
+}
+//while using array
+let a=0;
+var z=['blue','pink','black'];
+while(a<=z.length);
+{
+    console.log(z[i]);
+}
+
+//do while
+let start=1;
+do
+{
+    console.log(start);
+    start++;
+}
+while(start<=10);
